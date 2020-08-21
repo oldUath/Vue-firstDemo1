f782c6ae01adcdc1401031f6666ca9b48c8c8670
oldDong <2232890213@qq.com>
2020-08-21 20:08:38 +0800

    '内部属性和周期'

--- src/Demo.vue
+++ src/Demo.vue
@@ -1,28 +1,40 @@
-<template>
-<!-- 写视图 -->
-    <div class="red">
-        demo
-    </div>
-</template>
 
+<template>
+  <div class="red">
+    {{n}}
+    <button @click="add">+1</button>
+
+  </div>
+</template>
 <script>
-// 除了视图外的其他选项
 export default {
-    data(){
-        return {
-            n:0
-        }
-    },
-    methods:{
-        add(){
-            this.n+=1
-        }
+
+  data(){
+    return {
+      n:0,
+      array:[1,2,3,4,5,6,7,8]
     }
-};
+
+  },
+
+  created(){
+    console.log('出现在内存中，没有出现在页面中')
+  },
+  mounted() {
+    console.log('我已经出现在页面中')
+  },
+  updated() {
+    console.log('更新才会出现')
+  },
+  destroyed() {
+    console.log('已经消亡了')
+  },
+  methods:{
+    add(){
+      this.n+=1
+    }
+  }
+}
 </script>
 
-<style scoped>
-.red{
-    color:red;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -4,41 +4,26 @@ const Vue =window.Vue
 
 Vue.config.productionTip = false
 
-//引入的Demo就是一个组件
-import Demo from "./Demo.vue";
+import Demo from './Demo.vue'
 
-Vue.component('Demo2',{
-  template:`
-  <div>demo2方式二</div>
-  `
-})
 const vm = new Vue({
-  //告诉程序我要使用的组件是谁，和它对应的值
-  components:{
-    Frank:Demo
-  },
+  //不用render渲染的方法
+  components:{Demo},
   data:{
-    n:0,
-    array:[1,2,3,4,5,6,7,8]
+    visible:true
   },
   template:`
-    <div class="red">
-      {{n}}
-      <button @click="add">+1</button>
-      <Frank/>
-      <Demo2 />
-    <hr>
-    {{filter()}}
-    </div>
+      <div>
+        <button @click="toggle">toggle</button>
+        <hr>
+        <Demo v-if="visible === true" />
+      </div>
   `,
   methods:{
-    add(){
-      this.n+=1
-    },
-    filter(){
-      return this.array.filter(i=>i%2===0)
+    toggle(){
+      this.visible=!this.visible
     }
   }
-
+  // render: h => h(Demo)
 })
 vm.$mount('#frank')
